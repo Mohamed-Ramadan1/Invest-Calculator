cb0b48fb39fcebfe46edaa010508e4da140cec58
Mohamed Ramadan <mohamedramadan11b@gmail.com>
2023-06-13 23:59:36 +0300

    creating the form component

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import Form from './Components/Form';
 import Header from './Components/Header';
 
 
@@ -31,41 +32,11 @@ function App() {
 
   return (
     <div>
-    <Header/>
+      <Header />
+      <Form/>
   
 
-      <form className="form">
-        <div className="input-group">
-          <p>
-            <label htmlFor="current-savings">Current Savings ($)</label>
-            <input type="number" id="current-savings" />
-          </p>
-          <p>
-            <label htmlFor="yearly-contribution">Yearly Savings ($)</label>
-            <input type="number" id="yearly-contribution" />
-          </p>
-        </div>
-        <div className="input-group">
-          <p>
-            <label htmlFor="expected-return">
-              Expected Interest (%, per year)
-            </label>
-            <input type="number" id="expected-return" />
-          </p>
-          <p>
-            <label htmlFor="duration">Investment Duration (years)</label>
-            <input type="number" id="duration" />
-          </p>
-        </div>
-        <p className="actions">
-          <button type="reset" className="buttonAlt">
-            Reset
-          </button>
-          <button type="submit" className="button">
-            Calculate
-          </button>
-        </p>
-      </form>
+      
 
       {/* Todo: Show below table conditionally (only once result data is available) */}
       {/* Show fallback text if no data is available */}
